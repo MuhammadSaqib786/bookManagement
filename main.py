fc0ed65de72ac2ad196d615c2955c48b1f5b134f
Muhammad Saqib <66180927+MuhammadSaqib786@users.noreply.github.com>
2022-05-17 10:59:03 +0500

    complete program with read write update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import  os # module for deleting files from disk
+from datetime import date # for getting today date
 # This function is used to display menu to the user
 def menu():
     print("***************** Books Management ******************")
@@ -41,6 +43,7 @@ def find_book(book_number):
             if book_number == line.split(",")[0]:
                 flag = True
                 break
+    file.close()
     return flag
 # function to display books
 def display_books():
@@ -93,6 +96,81 @@ def display_selected_books(data):
     data[3].ljust(14, ' '), "|", data[4].ljust(4, ' '), "|", data[5].ljust(11, ' '), "|",
     data[6].ljust(14, ' '), "|")
     print("________________________________________________________________________________________________")
+
+# function to borrow a book
+def borrow_book():
+    print("________________________________________________________________________________________________")
+    book_number = input("Enter book number to borrow : ")
+    if find_book(book_number): # check if book available
+        file = open("books.txt", "r")
+        lines = file.readlines()
+        for line in lines:
+            if len(line) > 1:
+                data = line.rstrip('\n').split(",")
+                if data[0] == book_number :
+                    if int(data[6]) > 0:  #check if available copies are more than zero
+                        file.close()
+                        decrease_available_copies(book_number) # decrease availaable copies by 1
+                        add_transaction(book_number,data[1])
+                        break
+                    else:
+                        print("Sorry all books already issued! come back later..")
+        file.close()
+    else:
+        print(f"Sorry no book found with number : {book_number}")
+    print("________________________________________________________________________________________________")
+# function to decrease no of available copies by 1
+def decrease_available_copies(book_number):
+    file = open("books.txt", "r") # read books file
+    file1 = open("temp.txt", "w") # write updated data
+    lines = file.readlines()
+    for line in lines:
+        if len(line) > 1:
+            data = line.rstrip('\n').split(",")
+            if data[0] == book_number:
+                available_copies=int(data[6]) - 1 # decrease no of availabale copies
+                file1.write(data[0]+","+data[1]+","+data[2]+","+data[3]+","+str(data[4])+","+str(data[5])+","+str(available_copies)+"\n")
+            else:
+                file1.write(line)
+    file1.close()
+    file.close()
+    os.remove("books.txt")
+    os.rename("temp.txt","books.txt")
+# function to add transaction if book is borrowed
+def add_transaction(book_number,book_title):
+    date_borrow = str(date.today())
+    borrower_name = input(" Borrower name : ")
+    text=date_borrow+","+borrower_name+","+book_number+","+book_title+"\n"
+    write_to_file("transactions.txt",text)
+    print("Book borrowed successfully!!!")
+# function to return a book
+def return_book():
+    print("________________________________________________________________________________________________")
+    book_number = input("Enter book number to return : ")
+    if find_book(book_number):
+        increase_available_copies(book_number)
+    else:
+        print(f"Book with number {book_number} not found")
+    print("________________________________________________________________________________________________")
+# function to increase no of available copies by 1
+def increase_available_copies(book_number):
+    file = open("books.txt", "r")  # read books file
+    file1 = open("temp.txt", "w")  # write updated data
+    lines = file.readlines()
+    for line in lines:
+        if len(line) > 1:
+            data = line.rstrip('\n').split(",")
+            if data[0] == book_number and int(data[5]) > int(data[6]):
+                available_copies = int(data[6]) + 1  # increase no of availabale copies
+                file1.write(data[0] + "," + data[1] + "," + data[2] + "," + data[3] + "," + str(data[4]) + "," + str(
+                    data[5]) + "," + str(available_copies) + "\n")
+            else:
+                file1.write(line)
+    file1.close()
+    file.close()
+    os.remove("books.txt")
+    os.rename("temp.txt", "books.txt")
+    print("Book returned successfully")
 # main function
 if __name__ == '__main__':
     choice = menu()  # check the choice
@@ -109,11 +187,11 @@ if __name__ == '__main__':
         elif choice == 3:
             display_books()
         elif choice == 4:
-            pass
+            borrow_book()
         elif choice == 5:
-            pass
+            return_book()
         elif choice == 6:
-            pass
+            print("Bye!! Thanks for using.")
         else:
             print("Wrong input!!")
         choice = menu()
